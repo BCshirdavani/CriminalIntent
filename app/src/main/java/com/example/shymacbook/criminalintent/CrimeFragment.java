682601009efafe1a264e66d02a6abe7057eecf6c
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-02-23 11:11:58 -0800

    ch.7 - pp.147 - widgets wired
    widgets are connected to the fragmen.
    
    to do: add UI fragment to fragment manager. add crime fragment to crime activity...

--- app/src/main/java/com/example/shymacbook/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/shymacbook/criminalintent/CrimeFragment.java
@@ -3,9 +3,15 @@ package com.example.shymacbook.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 /**
  * Created by shymacbook on 2/23/18.
@@ -14,9 +20,12 @@ import android.view.ViewGroup;
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(/*@Nullable*/ Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
     }
@@ -26,6 +35,42 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, /*@Nullable*/ ViewGroup container, /*@Nullable*/ Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        // connecting the EditText widget
+         mTitleField = (EditText) v.findViewById(R.id.crime_title);
+         mTitleField.addTextChangedListener(new TextWatcher() {
+             @Override
+             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+             }
+
+             @Override
+             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                 mCrime.setTitle(s.toString());
+             }
+
+             @Override
+             public void afterTextChanged(Editable s) {
+
+             }
+         });
+
+         // connecting the Button widget
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);  // button is disabled until chapter 12
+
+        // connecting the CheckBox widget
+        // TODO : add on demand static import for 'android.widget.CompoundButton' - pp.147
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+
         return v;
     }
 }
