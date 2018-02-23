86ac956ebe746789bdf226dbe08b89212dc75bac
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-02-22 19:06:34 -0800

    ch.5 - pp.135 - Model for Crime created
    the project is ready to host a fragment, but all we've done so far is make a Crime class for our model.

---
+++ app/src/main/java/com/example/shymacbook/criminalintent/Crime.java
@@ -0,0 +1,49 @@
+package com.example.shymacbook.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by shymacbook on 2/22/18.
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
